1acbe8aa57d9bceb8003a5ac6c3a0dace102e7e9
sachin mondal <you@example.com>
2024-05-26 04:45:05 +0530

    get-a-chai

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,11 +3,13 @@ import AppleProvider from 'next-auth/providers/apple'
 import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
 import EmailProvider from 'next-auth/providers/email'
+import GithubProvider from 'next-auth/providers/github'
 
 
-export default NextAuth({
+export const authoption = NextAuth({
     providers: [
       // OAuth authentication providers...
+
       AppleProvider({
         clientId: process.env.APPLE_ID,
         clientSecret: process.env.APPLE_SECRET
@@ -27,3 +29,7 @@ export default NextAuth({
       }),
     ]
   })
+
+  export {authoption as GET, authoption as POST}
+
+  //57;;;
